ec93e67486c0be87dbeb5d05cd75ed3764689ee0
Alex Chubaty <alex.chubaty@gmail.com>
2020-03-30 15:26:03 -0600

    update my contact info

--- mpbRandomLandscapes.R
+++ mpbRandomLandscapes.R
@@ -6,7 +6,7 @@ defineModule(sim, list(
   description = "Mountain Pine Beetle Red Top Growth Model: Short-run Potential for Establishment, Eruption, and Spread",
   keywords = c("mountain pine beetle, outbreak dynamics, eruptive potential, spread, climate change, twitch response"),
   authors = c(
-    person(c("Alex", "M"), "Chubaty", email = "alexander.chubaty@canada.ca", role = c("aut", "cre"))
+    person(c("Alex", "M"), "Chubaty", email = "achubaty@for-cast.ca", role = c("aut", "cre"))
   ),
   childModules = character(),
   version = numeric_version("0.0.1"),
